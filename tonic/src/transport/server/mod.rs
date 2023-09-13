11477df8e9f1075b3383a3ddd6c2374e3e51be14
inkyu <gofiri@gmail.com>
2023-09-13 21:45:55 +0900

    Update

--- tonic/src/transport/server/mod.rs
+++ tonic/src/transport/server/mod.rs
@@ -526,23 +526,26 @@ pub trait ServeWithShutdown<L, S, I, F, IO, IE, ResBody>: Sized {
     ) -> Self::BoxFuture;
 }
 
-impl<L, S, I, F, IO, IE, ResBody> ServeWithShutdown<L, S, I, F, IO, IE, ResBody> for TokioExec
+macro_rules! define_serve_with_shutdown {
+($exec: ty, $box_future: tt $(, $maybe_send: tt)?) => {
+
+impl<L, S, I, F, IO, IE, ResBody> ServeWithShutdown<L, S, I, F, IO, IE, ResBody> for $exec
 where
     IO: AsyncRead + AsyncWrite + Connected + Unpin + Send + 'static,
-    ResBody: http_body::Body<Data = Bytes> + Send + 'static,
+    ResBody: http_body::Body<Data = Bytes> + $($maybe_send +)* 'static,
     ResBody::Error: Into<crate::Error>,
-    L: Layer<S> + Send + 'static,
-    L::Service: Service<Request<Body>, Response = Response<ResBody>> + Clone + Send + 'static,
-    <L::Service as Service<Request<Body>>>::Future: Send + 'static,
+    L: Layer<S> $(+ $maybe_send)* + 'static,
+    L::Service: Service<Request<Body>, Response = Response<ResBody>> + Clone + $($maybe_send +)* 'static,
+    <L::Service as Service<Request<Body>>>::Future:  $($maybe_send +)* 'static,
     <L::Service as Service<Request<Body>>>::Error: Into<crate::Error> + Send,
-    I: Stream<Item = Result<IO, IE>> + Send +'static,
+    I: Stream<Item = Result<IO, IE>> $(+ $maybe_send)* +'static,
     IO: AsyncRead + AsyncWrite + Connected + Unpin + Send + 'static,
     IO::ConnectInfo: Clone + Send + Sync + 'static,
     IE: Into<crate::Error> + 'static,
-    F: Future<Output = ()> + Send + 'static,
+    F: Future<Output = ()> $(+ $maybe_send)* + 'static,
     ResBody: http_body::Body<Data = Bytes>,
 {
-    type BoxFuture = crate::codegen::BoxFuture<(), super::Error>;
+    type BoxFuture = crate::codegen::$box_future<(), super::Error>;
 
     fn serve_with_shutdown(
         server: Server<Self, L>,
@@ -608,86 +611,11 @@ where
     }
 }
 
-impl<L, S, I, F, IO, IE, ResBody> ServeWithShutdown<L, S, I, F, IO, IE, ResBody> for LocalExec
-where
-    IO: AsyncRead + AsyncWrite + Connected + Unpin + Send + 'static,
-    ResBody: http_body::Body<Data = Bytes> + 'static,
-    ResBody::Error: Into<crate::Error>,
-    L: Layer<S> + 'static,
-    L::Service: Service<Request<Body>, Response = Response<ResBody>> + Clone + 'static,
-    <L::Service as Service<Request<Body>>>::Future: 'static,
-    <L::Service as Service<Request<Body>>>::Error: Into<crate::Error> + Send,
-    I: Stream<Item = Result<IO, IE>> + 'static,
-    IO: AsyncRead + AsyncWrite + Connected + Unpin + Send + 'static,
-    IO::ConnectInfo: Clone + Send + Sync + 'static,
-    IE: Into<crate::Error> + 'static,
-    F: Future<Output = ()> + 'static,
-{
-    type BoxFuture = crate::codegen::LocalBoxFuture<(), super::Error>;
-
-    fn serve_with_shutdown(
-        server: Server<Self, L>,
-        svc: S,
-        incoming: I,
-        signal: Option<F>,
-    ) -> Self::BoxFuture {
-        let trace_interceptor = server.trace_interceptor.clone();
-        let concurrency_limit = server.concurrency_limit;
-        let init_connection_window_size = server.init_connection_window_size;
-        let init_stream_window_size = server.init_stream_window_size;
-        let max_concurrent_streams = server.max_concurrent_streams;
-        let timeout = server.timeout;
-        let max_frame_size = server.max_frame_size;
-        let http2_only = !server.accept_http1;
-
-        let http2_keepalive_interval = server.http2_keepalive_interval;
-        let http2_keepalive_timeout = server
-            .http2_keepalive_timeout
-            .unwrap_or_else(|| Duration::new(DEFAULT_HTTP2_KEEPALIVE_TIMEOUT_SECS, 0));
-        let http2_adaptive_window = server.http2_adaptive_window;
-        let http2_max_pending_accept_reset_streams = server.http2_max_pending_accept_reset_streams;
-
-        let svc = server.service_builder.service(svc);
-        let exec = server.exec.clone();
-
-        let tcp = incoming::tcp_incoming(incoming, server);
-        let incoming = accept::from_stream::<_, _, crate::Error>(tcp);
-
-        let svc = MakeSvc {
-            inner: svc,
-            concurrency_limit,
-            timeout,
-            trace_interceptor,
-            _marker: PhantomData::<(Self, IO)>,
-        };
-
-        let builder = hyper::Server::builder(incoming)
-            .executor(exec)
-            .http2_only(http2_only)
-            .http2_initial_connection_window_size(init_connection_window_size)
-            .http2_initial_stream_window_size(init_stream_window_size)
-            .http2_max_concurrent_streams(max_concurrent_streams)
-            .http2_keep_alive_interval(http2_keepalive_interval)
-            .http2_keep_alive_timeout(http2_keepalive_timeout)
-            .http2_adaptive_window(http2_adaptive_window.unwrap_or_default())
-            .http2_max_pending_accept_reset_streams(http2_max_pending_accept_reset_streams)
-            .http2_max_frame_size(max_frame_size);
-
-        Box::pin(async move {
-            if let Some(signal) = signal {
-                builder
-                    .serve(svc)
-                    .with_graceful_shutdown(signal)
-                    .await
-                    .map_err(super::Error::from_source)?;
-            } else {
-                builder.serve(svc).await.map_err(super::Error::from_source)?;
-            }
-
-            Ok(())
-        })
-    }
 }
+}
+
+define_serve_with_shutdown!(TokioExec, BoxFuture, Send);
+define_serve_with_shutdown!(LocalExec, LocalBoxFuture);
 
 impl<Ex, L> Router<Ex, L> where Ex: HasBoxCloneService {
     pub(crate) fn new(server: Server<Ex, L>, routes: Routes<Ex>) -> Self
