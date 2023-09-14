3270ce46ab88a363aa2e8239e8ced8168c4e882d
Inkyu Lee <inkyu@prestolabs.io>
2023-09-14 20:44:19 +0900

    Update

--- tonic-health/src/generated/grpc_health_v1.rs
+++ tonic-health/src/generated/grpc_health_v1.rs
@@ -420,7 +420,7 @@ pub mod health_server {
                                 .status(200)
                                 .header("grpc-status", "12")
                                 .header("content-type", "application/grpc")
-                                .body(tonic::body::empty_body())
+                                .body(empty_body())
                                 .unwrap(),
                         )
                     })
